aa389f9c79a960046fa5bbb248c75d8d7b9ff86a
John Cipriano <johnmikecip@gmail.com>
2015-08-18 13:44:31 -0700

    fix dining assoc bug

--- app/models/dining.rb
+++ app/models/dining.rb
@@ -1,10 +1,11 @@
 class Dining < ActiveRecord::Base
-  belongs_to :user
   belongs_to :restaurant
+  belongs_to :diner, class_name: :User, foreign_key: :user_id, primary_key: :id
 
+  validates :diner, :restaurant, presence: true
   validates :favorite, inclusion: { in: [true, false] }
   validates :visited, inclusion: { in: [true, false] }
-  validates :user, :restaurant, :notes, presence: true
+  validates :restaurant, uniqueness: { scope: :diner }
 
   alias_attribute :favorite?, :favorite
   alias_attribute :visited?, :visited
