240b847863dd640c95fced6cebeaf6f1eca69e3a
John Cipriano <johnmikecip@gmail.com>
2015-08-18 13:48:36 -0700

    document resource_controller

--- app/controllers/concerns/resource_controller.rb
+++ app/controllers/concerns/resource_controller.rb
@@ -1,3 +1,4 @@
+# Common code for a generic resource controller.
 module ResourceController
   extend ActiveSupport::Concern
 
@@ -8,14 +9,25 @@ module ResourceController
     before_action :get_collection, only: [:index]
   end
 
+  # Creates an ivar for a collection of the controller's associated model
+  # instances.
+  #
+  # e.g. @restaurants = Restaurant.all
   def get_collection
     instance_variable_set(ivar_name.pluralize, self.class.model.all)
   end
 
+  # Creates an ivar for the model instance corresponding to the controller
+  # and the params hash id.
+  #
+  # e.g. @restaurant = Restaurant.find(params[:id])
   def find_model
     instance_variable_set(ivar_name, self.class.model.find(params[:id]))
   end
 
+  # Returns the usual name for an instance variable for this controller/model.
+  #
+  # e.g. @restaurant
   def ivar_name
     '@' + self.class.model.to_s.to_s.underscore
   end
