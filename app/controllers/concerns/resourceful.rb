e7bacc57315392876b632ae502bf906fc8a4b4d2
John Cipriano <johnmikecip@gmail.com>
2015-08-19 14:39:48 -0700

    rename resourceful concern

---
+++ app/controllers/concerns/resourceful.rb
@@ -0,0 +1,34 @@
+# Common code for a generic resource controller.
+module Resourceful
+  extend ActiveSupport::Concern
+
+  include LoginRequired
+
+  included do
+    before_action :find_model, only: [:show]
+    before_action :assign_collection, only: [:index]
+  end
+
+  # Creates an ivar for a collection of the controller's associated model
+  # instances.
+  #
+  # e.g. @restaurants = Restaurant.all
+  def assign_collection
+    instance_variable_set(ivar_name.pluralize, self.class.model.all)
+  end
+
+  # Creates an ivar for the model instance corresponding to the controller
+  # and the params hash id.
+  #
+  # e.g. @restaurant = Restaurant.find(params[:id])
+  def find_model
+    instance_variable_set(ivar_name, self.class.model.find(params[:id]))
+  end
+
+  # Returns the usual name for an instance variable for this controller/model.
+  #
+  # e.g. @restaurant
+  def ivar_name
+    '@' + self.class.model.to_s.to_s.underscore
+  end
+end
