4bf106742ec95103d3847cf80a64b29f257170b9
John Cipriano <johnmikecip@gmail.com>
2015-08-24 22:44:21 -0700

    restaurant bb model extra fields

--- app/assets/javascripts/models/restaurant.js
+++ app/assets/javascripts/models/restaurant.js
@@ -1,15 +1,32 @@
 FoodEx.Models.Restaurant = Backbone.Model.extend({
   urlRoot: 'api/restaurants',
 
-  listing: function () {
+  listing: function() {
     this._listing = this._listing ||
-      new FoodEx.Models.Listing({}, { restaurant: this });
+      new FoodEx.Models.Listing({}, {
+        restaurant: this
+      });
     return this._listing;
   },
 
+  lists: function() {
+    this._lists = this._lists || [];
+    return this._lists;
+  },
+
+  categories: function() {
+    this._categories = this._categories || [];
+    return this._categories;
+  },
+
   parse: function(payload) {
+    if (payload.categories) {
+      this._categories = payload.categories;
+    }
+    if (payload.lists) {
+      this._lists = payload.lists;
+    }
     if (payload.listing) {
-      // if we parse again at this stage then we are truly lost
       this.listing().set(payload.listing);
     }
     return payload;
